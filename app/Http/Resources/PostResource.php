8a67f8a9e9a74e70dc24ab2766a8584e246ac68f
Mohamed elnemr <mohamed.ahmed.aboelfath@gmail.com>
2019-03-15 10:47:24 +0200

    nested resources

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+//        dd($this->user);
+        return [
+            'post_id'=>$this->id,
+            'post_title'=>$this->title,
+            'description'=>$this->description,
+            'created_at'=>$this->created_at->toDateString(),
+//            'user_id'=>$this->user_id,
+
+        'user'=>new UserResource($this->user),
+    ];
+    }
+}
