3c55ac724f90640696fa7490fc99958e34dbff0e
Mohamed elnemr <mohamed.ahmed.aboelfath@gmail.com>
2019-03-15 09:54:28 +0200

    add postresource

---
+++ app/Http/Controllers/Api/PostsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Resources\PostResource;
+use App\Post;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PostsController extends Controller
+{
+    public function index(){
+        return PostResource::collection(Post::all());
+    }
+}
